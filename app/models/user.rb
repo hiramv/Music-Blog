b4d8ba6bdd8b4bfbc718631ba4fbef2ca58d59a0
hiramv <hiramv@gmail.com>
2013-08-12 18:31:38 -0400

    Deleted some unused files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  rolify
+	has_and_belongs_to_many :roles
+	has_many :posts
+
+	def role?(role)
+      return !!self.roles.find_by_name(role.to_s.camelize)
+  	end
+
+  	def self.from_omniauth(auth)
+  		where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+	end
+
+	def self.create_from_omniauth(auth)
+  		create! do |user|
+    		user.provider = auth["provider"]
+    		user.uid = auth["uid"]
+    		user.name = auth["info"]["nickname"]
+  		end
+	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
 end
