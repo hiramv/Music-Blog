bda281e3d451f6eaf9d1b14e6138ff1c476b161a
hiramv <hiramv@gmail.com>
2013-08-13 11:13:50 -0400

    Test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,2 @@
 class UsersController < ApplicationController
-  attr_accessible :provider, :uid, :name, :email
-  resourcify
-  
-  def index
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def show
-  end
 end
